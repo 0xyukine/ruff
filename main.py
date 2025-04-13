4452d7ecb60578594d6cb133542ac6478a73af80
0xyukine <44278060+0xyukine@users.noreply.github.com>
2025-04-13 23:29:31 +0100

    stuffy stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import hashlib
 
 def list_dir(dir_path='.', sort=True):
     directory = [x for x in Path(dir_path).iterdir()]
@@ -15,15 +16,12 @@ def compare_dirs(dir1='.', dir2='.'):
 
     while True:
         if dir1[x].name < dir2[y].name:
-            # print(count, dir1[x].name + "\t")
             yield (dir1[x].name, "")
             x += 1
         elif dir1[x].name > dir2[y].name:
-            # print(count, "\t"*2 + dir2[y].name)
             yield ("", dir2[y].name)
             y += 1
         elif dir1[x].name == dir2[y].name:
-            # print(count, dir1[x].name, dir2[y].name)
             yield (dir1[x].name, dir2[y].name)
             x += 1
             y += 1
@@ -31,13 +29,11 @@ def compare_dirs(dir1='.', dir2='.'):
 
         if x == dir1_max - 1:
             while y <= dir2_max - 1:
-                # print(count, "\t"*2 + dir2[y].name)
                 yield ("", dir2[y].name)
                 y += 1
             break
         if y == dir2_max - 1:
             while x <= dir1_max - 1:
-                # print(count, dir1[x].name)
                 yield (dir1[x].name, "")
                 x += 1
             break
@@ -46,12 +42,24 @@ def compare_dirs(dir1='.', dir2='.'):
 
     # print(x,y)
 
-dir1 = list_dir('/mnt/e/Stuff/Rips/twitter_old')
-dir2 = list_dir('/mnt/e/Stuff/Rips/twitter')
+def md5hash(file_bytes):
+    h = hashlib.new('md5')
+    h.update(file_bytes)
+    return h.hexdigest()
 
-compare_generator = compare_dirs(dir1, dir2)
-# for x in compare_generator:
-#     print(x)
+def perceptualhash():
+    pass
 
-while True:
-    print(next(compare_generator))
+def open_file(file_path):
+    with open(file_path, 'rb') as f:
+        file_bytes = f.read()
+    return file_bytes
+
+# dir1 = list_dir('/mnt/e/Stuff/Rips/twitter_old')
+# dir2 = list_dir('/mnt/e/Stuff/Rips/twitter')
+
+# compare_generator = compare_dirs(dir1, dir2)
+# for item_tuple in compare_generator:
+#     print(item_tuple)
+
+print(md5hash(open_file('')))
